72d4f1764865dc52102ca5aba381d6de118de103
Vova_Reznik <reznikvova2222@gmail.com>
2021-03-11 23:45:26 +0200

    Updates

---
+++ precache-manifest.0c694bc02a28c9b1eb40cbded853a853.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f2d37ca0ec06e10b855",
+    "url": "/game-of-throne/static/css/main.da92aad8.chunk.css"
+  },
+  {
+    "revision": "5f2d37ca0ec06e10b855",
+    "url": "/game-of-throne/static/js/main.5f2d37ca.chunk.js"
+  },
+  {
+    "revision": "912158eb3f6df14646cc",
+    "url": "/game-of-throne/static/css/1.86284387.chunk.css"
+  },
+  {
+    "revision": "912158eb3f6df14646cc",
+    "url": "/game-of-throne/static/js/1.912158eb.chunk.js"
+  },
+  {
+    "revision": "769cdb6ea882c25c7015",
+    "url": "/game-of-throne/static/js/runtime~main.769cdb6e.js"
+  },
+  {
+    "revision": "22389cf47ad284464c13506f2e22e8af",
+    "url": "/game-of-throne/static/media/error.22389cf4.jpg"
+  },
+  {
+    "revision": "4da810837c6dd45262462cccf9566c34",
+    "url": "/game-of-throne/static/media/got.4da81083.jpeg"
+  },
+  {
+    "revision": "003b8597172464ab5dde3ac5dcec43c0",
+    "url": "/game-of-throne/index.html"
+  }
+];
